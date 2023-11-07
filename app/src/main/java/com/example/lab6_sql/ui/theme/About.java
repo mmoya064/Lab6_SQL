3daaa3a0f51aa8d6124bec4b212e13055cc55356
mmoya064 <mmoya064@uottawa.ca>
2023-11-06 20:51:26 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab6_sql/ui/theme/About.java
@@ -0,0 +1,16 @@
+package com.example.lab6_sql.ui.theme;
+
+//package com.example.lab6demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class About extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_about);
+        }
+    }
